ec6587033656b06d171ff61dbe900cadba52c8df
kevinrios-cpu <80782564+kevinrios-cpu@users.noreply.github.com>
2022-03-20 15:22:12 -0300

    Update 1
    Implementacion de login y registro
    (Falta agregar  mapa y solucionar error de confirmar contraseña ya que no funciona el confirmar contraseña)

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms'
+import { AlertController, NavController } from '@ionic/angular';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formularioLogin: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl: NavController) {
+
+    this.formularioLogin = this.fb.group({
+      'patente': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+
+   }
+
+  ngOnInit() {
+  }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+
+    if(usuario.patente == f.patente && usuario.password == f.password){
+      console.log('Ingresado');
+      localStorage.setItem('ingresado','true');
+      this.navCtrl.navigateRoot('inicio');
+    }
+    else{
+      const alert = await this.alertController.create({
+        header: 'Datos icorrectos',
+        message: 'Los datos que ingresaste son incorrectos',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+    }
+  }
+}
