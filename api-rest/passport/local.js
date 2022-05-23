c4e05a44a13eb68c53dd39548e232d6522cd5792
kevinrios-cpu <80782564+kevinrios-cpu@users.noreply.github.com>
2022-05-23 16:11:33 -0400

    login api
    en proceso pero entregando datos luego de detectar que si existe el usuario y que la contraseña es correcta

--- api-rest/passport/local.js
+++ api-rest/passport/local.js
@@ -2,18 +2,17 @@ const LocalStrategy = require('passport-local').Strategy,
 { _finByUsername } = require('../controllers/users'),
 bcrypt = require('bcrypt');
 
-module.exports = new LocalStrategy({ session: false }, async (rutuser, password, done) => {
+module.exports = new LocalStrategy({ session: false }, async (username, password, done) => {
     try {
-        const parking_user = await _finByUsername(rutuser);
+        const parking_user = await _finByUsername(username);
         if (!parking_user) return done(null, false, 'Usuario o contraseña incorrectos');
         const match = bcrypt.compareSync(password, parking_user.password);
         if (!match) return done(null, false, 'Usuario o contraseña incorrectos');
         return done(null, {
-            rutuser: parking_user.rutuser,
-            id: parking_user.id,
+            username: parking_user.rutuser,
+            ID: parking_user.ID,
             created_at: parking_user.created_at,
-            created_at: parking_user.update_at,
-            created_at: parking_user.deleted_at
+            created_at: parking_user.update_at
         });
     } catch (e) {
         done(e);
--- api-rest/routes/auth.js
+++ api-rest/routes/auth.js
@@ -5,17 +5,17 @@ passport = require('passport'),
 jwt = require('jsonwebtoken');
 
 router.post('/login', async(req, res, next) => {
-    passport.authenticate('local', { session: false }, function(err, rutuser, info){
+    passport.authenticate('local', { session: false }, function(err, parking_user, info){
         if (err) return res.status(500).json(err);
-        if (!rutuser) return res.status(400).json(info);
-        const token = jwt.sign(rutuser, 'blabla', { expiresIn: '1h'});
+        if (!parking_user) return res.status(400).json(info);
+        const token = jwt.sign(parking_user, 'blabla', { expiresIn: '1h'});
         return res.status(200).json({
-            token, expiresIn: 3600, rutuser
+            token, expiresIn: 3600, parking_user
         });
     })(req, res, next);
 });
 
-router.post('/signup', async(req, res) =>{
+router.post('/registro', async(req, res) =>{
     try{
         const foundUser = await _finByUsername(req.body.rutuser);
         if (foundUser) {
